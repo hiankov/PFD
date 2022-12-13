53bad55d603c5d2cf8453ee8c1bab3ad1a2c64c2
hiankov <h.iankov@gmail.com>
2022-12-13 20:00:56 +0200

    Initial commit

---
+++ PFD/PFD/Controllers/ClientAreaController.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PFD.Models;
+
+namespace PFD.Controllers;
+
+public class ClientAreaController : Controller
+{
+    private readonly ILogger<ClientAreaController> _logger;
+
+    public ClientAreaController(ILogger<ClientAreaController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Dashboard()
+    {
+        return View();
+    }
+
+    public IActionResult Accounts()
+    {
+        return View();
+    }
+
+    public IActionResult AccPlans()
+    {
+        return View();
+    }
+    public IActionResult Transactions()
+    {
+        return View();
+    }
+    public IActionResult Reports()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
